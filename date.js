6b242f2802da5fa0437dac15368d6b38e017dc82
Akash <aku.das02@gmail.com>
2020-07-21 03:58:33 +0600

    added date fact api

--- date.js
+++ date.js
@@ -1,18 +1,16 @@
 let dateFact = document.querySelector('#dateFact');
 let dateFactText = document.querySelector('#dateFactText');
 
-let dayInput = document.querySelector('#dayInput');
-let monthInput = document.querySelector('#monthInput');
 let dateInput = document.querySelector('#dateInput');
 dateInput.addEventListener('input', getDateFactFetch);
 
 function getDateFactFetch() {
-    let number = `${monthInput.value}/${dayInput.value}`;
+    let number = dateInput.value;
 
     fetch('http://numbersapi.com/' + number + '/date')
         .then(response => response.text())
         .then(data => {
-            if (dayInput.value != "" && monthInput.value != "") {
+            if (number != "") {
                 dateFact.style.display = 'block';
                 dateFactText.innerText = data;
             } else {
